72abc49ae7aaf0c7c53ec50208a0ee62deebfd81
Irina <ioblomova13@gmail.com>
2021-10-19 13:46:31 +0300

    update countryModal

--- src/components/CountryModal.jsx
+++ src/components/CountryModal.jsx
@@ -1,7 +1,9 @@
-import { useState } from 'react';
 import Modal from 'react-modal';
 
 const customStyles = {
+    overlay: {
+        backgroundColor: 'rgba(0, 0, 0, 0.75)'
+    },
     content: {
         top: '50%',
         left: '50%',
@@ -9,13 +11,14 @@ const customStyles = {
         bottom: 'auto',
         marginRight: '-50%',
         transform: 'translate(-50%, -50%)',
+        borderRadius: '20px',
     },
 };
 
 Modal.setAppElement('#root');
 
 const CountryModal = (props) => {
-    const {country, open, closeModal} = props;
+    const { country, open, closeModal } = props;
 
     return (
         <>
@@ -24,11 +27,51 @@ const CountryModal = (props) => {
                 onRequestClose={closeModal}
                 style={customStyles}
             >
-                <div>{country.Country}</div>
-                <div>TotalConfirmed {country.TotalConfirmed}</div>
-                <div>TotalDeaths {country.TotalDeaths}</div>
-                <div>TotalRecovered {country.TotalRecovered}</div>
-                <button onClick={closeModal}>OK</button>
+                <div className="heading_modal">{country.Country}</div>
+                <div className="common_modal">
+                    <div className="left_right">
+                        <div className="left_total">
+                            <img className="svg"
+                                 src={`${process.env.PUBLIC_URL}/assets/confirmed.svg`}
+                                 alt="confirmed"
+                            />
+                            TotalConfirmed
+                        </div>
+                        <div className="right_number">
+                            {country.TotalConfirmed}
+                        </div>
+                    </div>
+                    <div className="left_right">
+                        <div className="left_total">
+                            <img className="svg"
+                                 src={`${process.env.PUBLIC_URL}/assets/deaths.svg`}
+                                 alt="deaths"
+                            />
+                            TotalDeaths
+                        </div>
+                        <div className="right_number">
+                            {country.TotalDeaths}
+                        </div>
+                    </div>
+                    <div className="left_right">
+                        <div className="left_total">
+                            <img className="svg"
+                                 src={`${process.env.PUBLIC_URL}/assets/recovered.svg`}
+                                 alt="recovered"
+                            />
+                            TotalRecovered
+                        </div>
+                        <div className="right_number">
+                            {country.TotalRecovered}
+                        </div>
+                    </div>
+                </div>
+                <div className="button_wrapper">
+                    <button
+                        className="button_modal"
+                        onClick={closeModal}> OK
+                    </button>
+                </div>
             </Modal>
         </>
     );
